fdcc9913c14a4336a5c72e24ffcaa4f4fcb09edd
Ning Sun <sunng@about.me>
2015-09-21 23:03:22 +0800

    (feat) select over kuchiki [#7]

--- src/page.rs
+++ src/page.rs
@@ -1,7 +1,7 @@
 use hyper::Client;
-use kuchiki::{Html, NodeRef, NodeDataRef, ElementData};
-use string_cache::namespace::{Namespace, QualName};
-use string_cache::atom::Atom;
+
+use select::dom::Dom;
+use select::predicate::*;
 
 #[derive(Debug)]
 pub struct DictDef {
@@ -27,78 +27,32 @@ fn urban_dict_url (word: &str) -> String {
     s
 }
 
-fn get_first_match(node: &NodeRef, selector: &str) -> Option<NodeDataRef<ElementData>> {
-    if let Ok(mut eles) = node.select(selector) {
-        eles.next()
-    } else {
-        None
-    }
-}
-
-fn get_attribute_from_node(panel: &NodeRef, attr_name: &str) -> Option<String> {
-    if let Some(panel_ele) = panel.as_element() {
-        let attr_map = panel_ele.attributes.borrow();
-        if let Some(attr_value) = attr_map.get(
-            &QualName::new(Namespace(Atom::from_slice("")), Atom::from_slice(attr_name))) {
-            return Some(attr_value.clone());
-        }
-    }
-    None
-}
-
-fn get_node_text(node_data: &NodeDataRef<ElementData>) -> String {
-    let node = node_data.as_node();
-    node.text_contents()
-}
-
 fn json_list_to_strings(json: &str) -> Vec<String> {
     json[1..json.len()-1].split(", ").map(|s| s[1..s.len()-1].to_owned()).collect()
 }
 
-fn get_def_from_doc (doc: &NodeRef) -> Option<DictDef> {
-    if let Some(panel_ele) = get_first_match(doc, "div.def-panel") {
-        let panel = panel_ele.as_node();
-        if let Some(word_ele) = get_first_match(panel, "a.word") {
-            let word_text = get_node_text(&word_ele);
+fn get_def_from_doc (doc: &Dom) -> Option<DictDef> {
+    if let Some(panel) = dom.find(Class("def-panel")).first() {
+        if let Some(word) = panel.find(Class("word")).first() {
+            let word = word.text();
 
-            let def_ele = get_first_match(&panel, "div.meaning").unwrap();
-            let def_text = get_node_text(&def_ele);
+            let def = panel.find(Class("meaning")).first().unwrap().text();
+            let defid = panel.attr("data-defid").unwrap_or("".to_owned());
 
-            let defid = get_attribute_from_node(panel, "data-defid").unwrap_or("".to_owned());
+            let example = panel.find(Class("example")).first().and_then(|e| Some(e.text())).unwrap_or("".to_owned());
+            let author = panel.find(Class("author")).first().and_then(|e| Some(e.text())).unwrap_or("".to_owned());
+            let date = panel.find(Class("author")).first().and_then(|e| e.next()).and_then(|e| Some(e.text())).unwrap_or("".to_owned());
+            let sounds = json_list_to_strings(&(panel.find(Class("play-sound")).first().and_then(|e| e.attr("data-urls")).unwrap_or("[]".to_owned())));
 
-            let example = if let Some(example_ele) = get_first_match(&panel, "div.example") {
-                Some(get_node_text(&example_ele))
-            } else {
-                None
-            };
-
-            let author_ele = get_first_match(&panel, "a.author").unwrap();
-            let author = get_node_text(&author_ele);
-            let date_node = author_ele.as_node().next_sibling().unwrap();
-            let date_text = date_node.as_text().unwrap().borrow().trim().to_owned();
-
-            let sounds = if let Some(sounds_ele) = get_first_match(&panel, "a.play-sound") {
-                let sounds_node = sounds_ele.as_node();
-                let sounds_json_list = get_attribute_from_node(&sounds_node, "data-urls").unwrap_or("[]".to_owned());
-                Some(json_list_to_strings(&sounds_json_list))
-            } else {
-                None
-            };
-
-            let similars = if let Ok(similar_eles) = doc.select("ul.alphabetical li a") {
-                Some(similar_eles.map(|item| item.as_node().text_contents()).collect())
-            } else {
-                None
-            };
-
+            let similars = doc.find(Name("ul").and(Class("alphabetical"))).find(Name("li")).find(Name("a")).iter().map(|e| e.text());
             Some(DictDef {
-                word: word_text.trim().to_owned(),
-                def: def_text.trim().to_owned(),
+                word: word.trim().to_owned(),
+                def: def.trim().to_owned(),
                 example: example,
                 upvote: 0,
                 downvote: 0,
                 contributor: author,
-                date: date_text,
+                date: date,
                 id: defid,
                 sounds: sounds,
                 similars: similars
@@ -115,13 +69,11 @@ fn find_from_url(url: &str) -> Option<DictDef> {
     let client = Client::new();
 
     let mut response = client.get(url).send().unwrap();
+    let mut body = String::new();
+    resp.read_to_string(&mut body).unwrap();
 
-    if let Ok(html) = Html::from_stream(&mut response) {
-        let doc = html.parse();
-        get_def_from_doc(&doc)
-    } else {
-        None
-    }
+    let dom = Dom::from_str(body);
+    get_def_from_doc(&dom)
 }
 
 pub fn find_word_of_the_day() -> Option<DictDef> {
