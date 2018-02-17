b9f19aaeb041f3df3808ba9cd3fd333a03ef0c00
Ning Sun <sunng@protonmail.com>
2018-02-17 16:25:03 +0800

    (fix) author field
    Signed-off-by: Ning Sun <sunng@protonmail.com>

--- src/page.rs
+++ src/page.rs
@@ -16,13 +16,12 @@ pub struct DictDef {
     pub date: String,
     pub id: String,
     pub sounds: Option<Vec<String>>,
-    pub similars: Option<Vec<String>>
+    pub similars: Option<Vec<String>>,
 }
 
-impl DictDef {
-}
+impl DictDef {}
 
-fn urban_dict_url (word: &str) -> String {
+fn urban_dict_url(word: &str) -> String {
     let mut s = String::new();
     s.push_str("https://www.urbandictionary.com/define.php?term=");
     s.push_str(word);
@@ -30,24 +29,65 @@ fn urban_dict_url (word: &str) -> String {
 }
 
 fn json_list_to_strings(json: &str) -> Vec<String> {
-    json[1..json.len()-1].split(", ").map(|s| s[1..s.len()-1].to_owned()).collect()
+    json[1..json.len() - 1]
+        .split(", ")
+        .map(|s| s[1..s.len() - 1].to_owned())
+        .collect()
 }
 
-fn get_def_from_doc (doc: &Document) -> Option<DictDef> {
+fn get_def_from_doc(doc: &Document) -> Option<DictDef> {
     if let Some(panel) = doc.find(Class("def-panel")).into_selection().first() {
         if let Some(word) = panel.find(Class("word")).into_selection().first() {
             let word = word.text();
 
-            let def = panel.find(Class("meaning")).into_selection().first().unwrap().text();
+            let def = panel
+                .find(Class("meaning"))
+                .into_selection()
+                .first()
+                .unwrap()
+                .text();
             let defid = panel.attr("data-defid").unwrap_or("").to_owned();
 
-            let example = panel.find(Class("example")).into_selection().first().and_then(|e| Some(e.text()));
-            let author = panel.find(Class("author")).into_selection().first().and_then(|e| Some(e.text())).unwrap_or("".to_owned());
-            let date = panel.find(Class("author")).into_selection().first().and_then(|e| e.next()).and_then(|e| Some(e.text().trim().to_owned())).unwrap_or("".to_owned());
+            let example = panel
+                .find(Class("example"))
+                .into_selection()
+                .first()
+                .and_then(|e| Some(e.text()));
+            let author = panel
+                .find(Class("contributor"))
+                .into_selection()
+                .first()
+                .and_then(|c| {
+                    c.find(Name("a"))
+                        .into_selection()
+                        .first()
+                        .and_then(|e| Some(e.text()))
+                })
+                .unwrap_or("".to_owned());
+            let date = panel
+                .find(Class("contributor"))
+                .into_selection()
+                .first()
+                .and_then(|e| e.last_child())
+                .and_then(|e| Some(e.text().trim().to_owned()))
+                .unwrap_or("".to_owned());
 
-            let sounds = panel.find(Class("play-sound")).into_selection().first().and_then(|e| e.attr("data-urls").and_then(|s| Some(s.to_owned()))).and_then(|l| Some(json_list_to_strings(&l)));
+            let sounds = panel
+                .find(Class("play-sound"))
+                .into_selection()
+                .first()
+                .and_then(|e| e.attr("data-urls").and_then(|s| Some(s.to_owned())))
+                .and_then(|l| Some(json_list_to_strings(&l)));
 
-            let similars = Some(doc.find(Name("ul").and(Class("alphabetical"))).into_selection().find(Name("li")).find(Name("a")).iter().map(|e| e.text()).collect());
+            let similars = Some(
+                doc.find(Name("ul").and(Class("alphabetical")))
+                    .into_selection()
+                    .find(Name("li"))
+                    .find(Name("a"))
+                    .iter()
+                    .map(|e| e.text())
+                    .collect(),
+            );
 
             Some(DictDef {
                 word: word.trim().to_owned(),
@@ -59,7 +99,7 @@ fn get_def_from_doc (doc: &Document) -> Option<DictDef> {
                 date: date,
                 id: defid,
                 sounds: sounds,
-                similars: similars
+                similars: similars,
             })
         } else {
             None
@@ -90,7 +130,7 @@ pub fn get_random_word() -> Option<DictDef> {
     find_from_url("https://www.urbandictionary.com/random.php")
 }
 
-pub fn find_on_urban_dict (word: &str) -> Option<DictDef> {
+pub fn find_on_urban_dict(word: &str) -> Option<DictDef> {
     let url = urban_dict_url(word);
     find_from_url(&url)
 }
